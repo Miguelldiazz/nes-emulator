58e43957b233436ac9c3333ce1d282d45d9c7cec
Your Name <you@example.com>
2020-04-24 16:49:50 +0200

    instructions

--- src/cpu.rs
+++ src/cpu.rs
@@ -13,7 +13,7 @@ impl Registers {
             a: 0x0,
             x: 0x0,
             y: 0x0,
-            pc: 0x0,
+            pc: 0x8000,
             sp: 0xfd,
             p: 0x34,
         }
@@ -179,6 +179,14 @@ impl Cpu {
         }
     }
 
+    fn bne(&mut self) {
+        let jump = self.mem.data[self.regs.pc as usize];
+        self.regs.pc += 1;
+        if self.regs.p & 0x02 != 0x02 {
+            self.regs.pc += jump as u16;
+        }
+    }
+
     pub fn next_instruction(&mut self) {
         let opcode = self.mem.data[self.regs.pc as usize];
         self.regs.pc += 1;
@@ -263,6 +271,183 @@ impl Cpu {
             0xb0 => self.bcs(),
             //BEQ
             0xf0 => self.beq(),
+            //BIT
+            0x24 => (),
+            0x2c => (),
+            //BMI
+            0x30 => (),
+            //BNE
+            0xd0 => self.bne(),
+            //BPL
+            0x10 => (),
+            //BRK
+            0x00 => (),
+            //BVC
+            0x50 => (),
+            //BVS
+            0x70 => (),
+            //CLC
+            0x18 => (),
+            //CLD
+            0xd8 => (),
+            //CLI
+            0x58 => (),
+            //CLV
+            0xb8 => (),
+            //CMP
+            0xc9 => (),
+            0xc5 => (),
+            0xd5 => (),
+            0xcd => (),
+            0xdd => (),
+            0xd9 => (),
+            0xc1 => (),
+            0xd1 => (),
+            //CPX
+            0xe0 => (),
+            0xe4 => (),
+            0xec => (),
+            //CPY
+            0xc0 => (),
+            0xc4 => (),
+            0xcc => (),
+            //DEC
+            0xc6 => (),
+            0xd6 => (),
+            0xce => (),
+            0xde => (),
+            //DEX
+            0xca => (),
+            //DEY
+            0x88 => (),
+            //EOR
+            0x49 => (),
+            0x45 => (),
+            0x55 => (),
+            0x4d => (),
+            0x5d => (),
+            0x59 => (),
+            0x41 => (),
+            0x51 => (),
+            //INC
+            0xe6 => (),
+            0xf6 => (),
+            0xee => (), 
+            0xfe => (),
+            //INX
+            0xe8 => (),
+            //INY
+            0xc8 => (),
+            //JMP
+            0x4c => (),
+            0x6c => (),
+            //JSR
+            0x20 => (),
+            //LDA
+            0xa9 => (),
+            0xa5 => (),
+            0xb5 => (),
+            0xad => (),
+            0xbd => (),
+            0xb9 => (),
+            0xa1 => (),
+            0xb1 => (),
+            //LDX
+            0xa2 => (),
+            0xa6 => (),
+            0xb6 => (),
+            0xae => (),
+            0xbe => (),
+            //LDY
+            0xa0 => (),
+            0xa4 => (),
+            0xb4 => (),
+            0xac => (),
+            0xbc => (),
+            //LSR
+            0x4a => (),
+            0x46 => (),
+            0x56 => (),
+            0x4e => (),
+            0x5e => (),
+            //NOP
+            0xea => (),
+            //ORA
+            0x09 => (),
+            0x05 => (),
+            0x15 => (),
+            0x0d => (), 
+            0x1d => (),
+            0x19 => (),
+            0x01 => (),
+            0x11 => (),
+            //PHA
+            0x48 => (),
+            //PHP
+            0x08 => (),
+            //PLA
+            0x68 => (),
+            //PLP
+            0x28 => (),
+            //ROL
+            0x2a => (),
+            0x26 => (),
+            0x36 => (),
+            0x2e => (),
+            0x3e => (),
+            //ROR
+            0x6a => (),
+            0x66 => (),
+            0x76 => (),
+            0x6e => (),
+            0x7e => (),
+            //RTI
+            0x40 => (),
+            //RTS
+            0x60 => (),
+            //SBC
+            0xe9 => (),
+            0xe5 => (),
+            0xf5 => (),
+            0xed => (),
+            0xfd => (),
+            0xf9 => (),
+            0xe1 => (),
+            0xf1 => (),
+            //SEC
+            0x38 => (),
+            //SED
+            0xf8 => (),
+            //SEI
+            0x78 => (),
+            //STA
+            0x85 => (),
+            0x95 => (),
+            0x8d => (),
+            0x9d => (),
+            0x99 => (),
+            0x81 => (),
+            0x91 => (),
+            //STX
+            0x86 => (),
+            0x96 => (),
+            0x8e => (),
+            //STY
+            0x84 => (),
+            0x94 => (),
+            0x8c => (),
+            //TAX
+            0xaa => (),
+            //TAY
+            0xa8 => (),
+            //TSX
+            0xba => (),
+            //TXA
+            0x8a => (),
+            //TXS
+            0x9a => (),
+            //TYA
+            0x98 => (),
             _ => println!("Error"),
         }
     }
