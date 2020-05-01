1df77da413cb6add9591abec10bcaa61555720a8
Your Name <you@example.com>
2020-05-01 16:09:02 +0200

    memory write

--- src/memory.rs
+++ src/memory.rs
@@ -10,4 +10,8 @@ impl Memory {
     pub fn read(&self, addr: u16) -> u8 {
         self.data[addr as usize]
     }
+
+    pub fn write(&mut self, addr: u16, value: u8) {
+        self.data[addr as usize] = value;
+    }
 }
